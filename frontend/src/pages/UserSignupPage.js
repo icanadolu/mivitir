5eda995e4600e4e248fbdf72fd0c02263764629f
Okan Erşan <yr504310@yargitay.gov.tr>
2023-02-02 22:16:03 +0300

    validation manuel

---
+++ frontend/src/pages/UserSignupPage.js
@@ -0,0 +1,97 @@
+import React from "react";
+import{signup} from '../api/apiCalls';
+
+class UserSignupPage extends React.Component {
+
+    state={
+        username:null,
+        displayName:null,
+        password: null,
+        passwordRepeat: null,
+        pendingApiCall:false,
+        errors:{}
+    }
+
+    onChange=event=>{
+        const {name,value} = event.target; 
+        const errors = {...this.state.errors}
+        errors[name] =undefined;
+        this.setState({
+            [name]:value,
+            errors
+        });
+    }
+
+   onclickSignup = async event =>{
+    event.preventDefault();
+    const{username,displayName,password} = this.state;
+    const body ={
+        username,
+        displayName,
+        password
+    }
+
+    this.setState({pendingApiCall:true});
+
+//   signup(body)
+//     .then(response=>{
+//         this.setState({pendingApiCall:false});
+//     }).catch(error=>{
+//         this.setState({pendingApiCall:false})
+//     });
+
+try {
+    
+    const response = await signup(body);
+} catch (error) {
+   if(error.response.data.validationErrors){
+    this.setState({errors:error.response.data.validationErrors});
+   }
+  
+}
+this.setState({pendingApiCall:false});
+   }
+
+    render() {
+        const{pendingApiCall,errors} = this.state;
+        const{username} = errors;
+
+        return (
+            <div className="container">
+                <form>
+                <h1 className="text-center">Sign Up</h1>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input className={username?"form-control is-invalid":"form-control"} name="username" onChange={this.onChange} />
+                    <div className="invalid-feedback">
+                       {username}
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label>Display Name</label>
+                    <input className="form-control" name="displayName"  onChange={this.onChange} />
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input className="form-control" name="password" type="password"  onChange={this.onChange}  />
+                </div>
+                <div className="form-group">
+                    <label>Password Repeat</label>
+                    <input className="form-control" name="passwordRepeat" type="password"  onChange={this.onChange}  />
+                </div>
+                <div className="text-center">
+                    <button className="btn btn-primary"
+                 disabled={pendingApiCall}
+                     onClick={this.onclickSignup}>
+                        {pendingApiCall&& <span className="spinner-border spinner-border-sm" ></span>}
+                         Sign Up</button>
+                </div>
+            </form>
+
+            </div>
+            
+        );
+    }
+}
+
+export default UserSignupPage;
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,13 +1,19 @@
 package com.hoaxify.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 
 @RestController
@@ -21,11 +27,21 @@ public class UserController {
 	
 	//@CrossOrigin
 	@PostMapping("/api/1.0/users")
-	public GenericResponse createUser(@RequestBody User user) {
-		userService.save(user);
-		GenericResponse response = new GenericResponse(" user created");
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+		String username= user.getUsername();
+		if(username ==null ||username.isEmpty()) {
+			ApiError error = new ApiError(400, "validation errors", "/api/1.0/users");
+		Map<String, String> validationErros = new HashMap<String, String>();
+		validationErros.put("username", "username cannot be null");
+		error.setValidationErrors(validationErros);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		}
 		
-		return response;
+		
+		userService.save(user);
+		
+		
+		return ResponseEntity.ok(new GenericResponse(" user created"));
 	}
 	
 }
