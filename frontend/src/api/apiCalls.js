33469fbde87959f2ad068656bf5a7d88041d870f
Okan Erşan <yr504310@yargitay.gov.tr>
2023-02-21 11:35:09 +0300

    frontend tarafında translation ve backend tarafında translatio ve  header default langue  özelleigi ile tüm istekleri defatul dil bilgisini backend gönderme çalışması

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -2,5 +2,10 @@ import axios from 'axios';
 
 
 export const signup = (body)=>{
+  //   return axios.post('/api/1.0/users',body,{header:{'accept-language': 'eng' }});
    return axios.post('/api/1.0/users',body);
+}
+
+export const changeLanguage = language =>{
+  axios.default.headers['accept-language'] = language;
 }
--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -1,6 +1,7 @@
 import React from "react";
-import{signup} from '../api/apiCalls';
+import{signup,changeLanguage} from '../api/apiCalls';
 import Input from "../components/Input";
+import {withTranslation} from 'react-i18next';
 
 class UserSignupPage extends React.Component {
 
@@ -14,15 +15,16 @@ class UserSignupPage extends React.Component {
     }
 
     onChange=event=>{
+      const{t} = this.props;
         const {name,value} = event.target;
         const errors = {...this.state.errors}
         errors[name] =undefined;
 
         if(name==='password'||name ==='passwordRepeat'){
           if (name==='password' && value !== this.state.passwordRepeat) {
-            errors.passwordRepeat ='Password mismatch';
+            errors.passwordRepeat =t('Password mismatch');
           }else if (name==='passwordRepeat' && value !== this.state.password) {
-            errors.passwordRepeat ='Password mismatch';
+            errors.passwordRepeat =t('Password mismatch');
           }
           else {
             errors.passwordRepeat = undefined;
@@ -64,33 +66,43 @@ class UserSignupPage extends React.Component {
     this.setState({pendingApiCall:false});
    }
 
+   onChangeLanguage = language =>{
+     const {i18n} = this.props;
+     i18n.changeLanguage(language);
+     changeLanguage(language);
+   }
+
     render() {
         const{pendingApiCall,errors} = this.state;
         const{username,displayName,password,passwordRepeat} = errors;
+        const{t} = this.props;
 
         return (
             <div className="container">
                 <form>
-                <h1 className="text-center">Sign Up</h1>
+                <h1 className="text-center">{t('Sign Up')} </h1>
 
-                <Input name="username" label="Username" error={username}  onChange={this.onChange} />
-                <Input name="displayName" label="Display Name" error={displayName}  onChange={this.onChange} />
-                <Input name="password" label="Password" error={password}  onChange={this.onChange}  type="password"/>
-                <Input name="passwordRepeat" label="Password Repeat" error={passwordRepeat}  onChange={this.onChange}   type="password"/>
+                <Input name="username" label={t("Username")} error={username}  onChange={this.onChange} />
+                <Input name="displayName" label={t("Display Name")} error={displayName}  onChange={this.onChange} />
+                <Input name="password" label={t("Password")} error={password}  onChange={this.onChange}  type="password"/>
+                <Input name="passwordRepeat" label={t("Password Repeat")} error={passwordRepeat}  onChange={this.onChange}   type="password"/>
 
                 <div className="text-center">
                     <button className="btn btn-primary"
                  disabled={pendingApiCall || passwordRepeat !== undefined}
                      onClick={this.onclickSignup}>
                         {pendingApiCall&& <span className="spinner-border spinner-border-sm" ></span>}
-                         Sign Up</button>
+                        {t('Sign Up')}</button>
                 </div>
             </form>
-
+            <span onClick={()=>  this.onChangeLanguage('tr')} style={{cursor:'pointer'}} >TR  </span>
+            <span onClick={()=>  this.onChangeLanguage('en')}  style={{cursor:'pointer'}}>EN  </span>
             </div>
 
         );
     }
 }
 
-export default UserSignupPage;
+const UserSignupPageWithTranslation = withTranslation()(UserSignupPage);
+
+export default UserSignupPageWithTranslation;
