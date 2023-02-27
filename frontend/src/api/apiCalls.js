bb9eb16260eeabba83121a88ae730cb22c2ffcb9
Okan Erşan <yr504310@yargitay.gov.tr>
2023-02-27 15:13:06 +0300

    credential işlemleri

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -6,6 +6,10 @@ export const signup = (body)=>{
    return axios.post('/api/1.0/users',body);
 }
 
+export const login = creds =>{
+  return axios.post('/api/1.0/auth',{},{auth:  creds});
+}
+
 export const changeLanguage = language =>{
   axios.default.headers['accept-language'] = language;
 }
