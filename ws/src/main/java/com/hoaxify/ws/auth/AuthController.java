312a9ba9cfb27bc3b228094255486bb5b9681f3c
icanadolu <ilimbilmektir@gmail.com>
2023-03-07 14:37:57 +0300

    springin paketi log seviyesi httpBasic().authenticationEntryPoint tanımlamasıyla auhetication popup göstermesini ezdik configure AuthenticationManagerBuilder ile auhetion servicemizin kullanarak login sagladık filterden dönen hataların generikhata tipine çevirerek gönderdik

--- ws/src/main/java/com/hoaxify/ws/auth/AuthController.java
+++ ws/src/main/java/com/hoaxify/ws/auth/AuthController.java
@@ -5,16 +5,12 @@ import java.util.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.Views;
 import com.hoaxify.ws.user.User;
 import com.hoaxify.ws.user.UserRepository;
@@ -27,38 +23,16 @@ public class AuthController {
 	@Autowired
 	UserRepository userRepository;
 
-	PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
 	@PostMapping("/api/1.0/auth")
 	@JsonView(Views.Base.class)
-	ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization", required = false) String authorization) {
-		if (authorization == null) {
-			ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
+	ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization") String authorization) {
+
 		String base64encoded = authorization.split("Basic ")[1];
 		String decoded = new String(Base64.getDecoder().decode(base64encoded));
 		String[] parts = decoded.split(":");
 		String username = parts[0];
 		String password = parts[1];
-		log.info(authorization);
 		User inDB = userRepository.findByUsername(username);
-
-		if (inDB == null) {
-			ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
-
-		String hashedPassword = inDB.getPassword();
-		if (!passwordEncoder.matches(password, hashedPassword)) {
-			ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
-
-		/*
-		 * Map<String, String> responseBody = new HashMap<>(); responseBody.put("username", inDB.getUsername()); responseBody.put("displayName", inDB.getDisplayName()); responseBody.put("image",
-		 * inDB.getImage());
-		 */
 
 		return ResponseEntity.ok(inDB);
 
--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,30 +1,25 @@
 package com.hoaxify.ws.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	
+
 	UserRepository userRepository;
 	PasswordEncoder passwordEncoder;
-	
-	
-	public UserService(UserRepository userRepository) {
-		
+
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+
 		this.userRepository = userRepository;
-		passwordEncoder = new BCryptPasswordEncoder();
+		this.passwordEncoder = passwordEncoder;
 	}
-
-
 
 	public void save(User user) {
 		String encryptedPassword = this.passwordEncoder.encode(user.getPassword());
 		user.setPassword(encryptedPassword);
-		userRepository.save(user); 
-		
+		userRepository.save(user);
+
 	}
-	
+
 }
