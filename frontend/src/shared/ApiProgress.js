623418b21939ec2e80c9f289e4762e6757032c05
icanadolu <ilimbilmektir@gmail.com>
2023-04-03 15:36:01 +0300

    higherorder componetiyle sarmala yaparak, properyleri passlanması, highorder bir classın anonimis sekilşde fonksiyondan return edilmesidir

--- frontend/src/shared/ApiProgress.js
+++ frontend/src/shared/ApiProgress.js
@@ -1,57 +1,65 @@
 import React, { Component } from 'react';
 import axios  from 'axios';
 
-class ApiProgress extends Component {
-    state ={
-        pendingApiCall: false
+
+export function withApiProgress(WrappedComponent, apiPath){
+    return class  extends Component {
+
+        static displayName ="ApiProgress";
+
+        state ={
+            pendingApiCall: false
+        }
+    
+    
+        componentDidMount(){
+            axios.interceptors.request.use((request)=>{
+                // if(request.url ===this.props.path){
+                //     this.setState({pendingApiCall:true});
+    
+                // }
+                this.updateApiCallFor(request.url,true);
+               return request;
+            });
+         
+            axios.interceptors.response.use((response)=>{
+            //    if(response.config.url === this.props.path){
+            //        this.setState({pendingApiCall:false});
+                    
+            //     }
+            this.updateApiCallFor(response.config.url,false);
+             return response;
+            },(error)=>{
+                // if(error.config.url === this.props.path){
+                //     this.setState({pendingApiCall:false});
+                     
+                //  }
+                this.updateApiCallFor(error.config.url,false);
+             throw error;
+            });
+           }
+    
+    updateApiCallFor = (url,inProgress) =>{
+        if(url === apiPath){
+            this.setState({pendingApiCall:inProgress});
+         }
     }
+    
+        render() {
+            const {pendingApiCall} = this.state;
+            // return (
+            //     <div>
+            //       {
+            //         React.cloneElement( this.props.children,{
+            //           pendingApiCall 
+            //         })
+            //       }
+            //     </div>
+            // );
 
-
-    componentDidMount(){
-        axios.interceptors.request.use((request)=>{
-            // if(request.url ===this.props.path){
-            //     this.setState({pendingApiCall:true});
-
-            // }
-            this.updateApiCallFor(request.url,true);
-           return request;
-        });
-     
-        axios.interceptors.response.use((response)=>{
-        //    if(response.config.url === this.props.path){
-        //        this.setState({pendingApiCall:false});
-                
-        //     }
-        this.updateApiCallFor(response.config.url,false);
-         return response;
-        },(error)=>{
-            // if(error.config.url === this.props.path){
-            //     this.setState({pendingApiCall:false});
-                 
-            //  }
-            this.updateApiCallFor(error.config.url,false);
-         throw error;
-        });
-       }
-
-updateApiCallFor = (url,inProgress) =>{
-    if(url === this.props.path){
-        this.setState({pendingApiCall:inProgress});
-     }
+            return <WrappedComponent pendingApiCall={pendingApiCall}  {...this.props}  />
+        }
+    }
 }
 
-    render() {
-        const {pendingApiCall} = this.state;
-        return (
-            <div>
-              {
-                React.cloneElement( this.props.children,{
-                  pendingApiCall 
-                })
-              }
-            </div>
-        );
-    }
-}
 
-export default ApiProgress;
